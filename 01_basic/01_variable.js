a0de95a089fb16fe0b627e2533ef4849c63e512a
adilsts <149329789+adilsts@users.noreply.github.com>
2024-04-02 07:35:52 +0000

    variable done

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,9 @@
+const account_Id = 123
+let account_Email = "adil.pakscribe.online"
+var account_Password = "5225"
+accounty_City = "Mardan"
+
+// account_Id = 123 // not allowed
+
+console.log(account_Id);
+console.table([account_Id , account_Email , account_Password , accounty_City]);
