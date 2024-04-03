8a979ab6d52ddb5f07d945a5419e794c87e523a4
adilsts <149329789+adilsts@users.noreply.github.com>
2024-04-03 17:05:35 +0000

    Conversion of datatypes

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,24 @@
+ // let score = 33
+
+// console.log(typeof score)
+// console.log(typeof(score))
+
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+
+/// "33" => 33
+// "33abc" => NaN
+// "true" => 0 , "false" => 1
+
+let isLoggedIn = 1
+
+let booleanIsloggedIn = Boolean(isLoggedIn)
+console.log(booleanIsloggedIn)
+
+// false = 0 , true = 1
+// "" = false , "text" = true
+
+let someNumber = 45
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
