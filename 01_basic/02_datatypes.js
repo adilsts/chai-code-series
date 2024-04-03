04032e2a5d571925fc3b3db870d473b4d0dffe02
adilsts <149329789+adilsts@users.noreply.github.com>
2024-04-03 07:57:36 +0000

    Dataypes done

---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,18 @@
+"use strict" // treat all js code for new all new version
+
+// alert(3+3) // we are using node.js , not browser
+
+console.log(3+3) // code readibility should be high
+
+let name = "Adil"
+let age = 26
+let isLoggedIn = false
+
+// number => 2 to power 53
+// BigInt
+// string => ""
+// boolean => false/true
+// null=> standaylone value
+// undefined =>
+console.log (typeof undefined);   // undefined
+console.log(typeof null);  // object
