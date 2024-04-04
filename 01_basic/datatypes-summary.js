3bde99b7a7db2c4d08a5fd6ae379eae9974700f2
adilsts <149329789+adilsts@users.noreply.github.com>
2024-04-04 09:52:17 +0000

    datatype-summary

---
+++ 01_basic/datatypes-summary.js
@@ -0,0 +1,29 @@
+// primitive 
+
+// 7 types : String, Number, Boolean, null, undefined, symbol, BigInt
+
+
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+//console.log(id === anotherId);
+
+const bigNumber = 255645345351563165315n
+
+//// console.log(bigNumber)
+
+// Non Primitive (Reference)
+
+// Array, Objects, Function
+
+const heros = "adil, ubaid, jillani";
+let myObj = {
+    name : "adil",
+    age : 26,
+
+}
+
+const myFunction = function ( ){
+    console.log("Hello world")
+}
