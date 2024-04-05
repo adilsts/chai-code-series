02223830e310b5b515c856a0fd3fa19f1f8495cf
adilsts <149329789+adilsts@users.noreply.github.com>
2024-04-05 12:52:12 +0000

    Strings are done

---
+++ 01_basic/06_strings.js
@@ -0,0 +1,15 @@
+const name = "Adil"
+const repoCount = 50
+
+/// console.log(name + repoCount + "  value");
+
+// console.log(`hi ${name} kaise ho ap k roll no ${repoCount}`)
+
+const gameNumber = new String("Adil");
+// console.log(gameNumber.toUpperCase())
+// console.log(gameNumber.charAt(2))
+// console.log(gameNumber.indexOf("d"))
+
+const newString = gameNumber.substring(0, 2);
+
+console.log(newString)
