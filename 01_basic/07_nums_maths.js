27b60d09f6ace19375dbaf2bef8f50347b0427d6
adilsts <149329789+adilsts@users.noreply.github.com>
2024-04-05 13:24:59 +0000

    Math and Nums are done

---
+++ 01_basic/07_nums_maths.js
@@ -0,0 +1,30 @@
+// const score = 400
+// const balance = new Number (100)
+// console.log(balance)
+
+// console.log(balance.toString().length)
+// console.log(balance.toFixed(2))
+
+// const otherNumber = 23.89516
+
+// console.log(otherNumber.toPrecision(4))
+// const hundreds = 1000000
+
+// console.log(hundreds.toLocaleString());
+
+
+//----------------Maths-------------------//
+// console.log(Math)
+// console.log(Math.abs(-55))
+// console.log(Math.round(4.9))
+// console.log(Math.ceil(4.2))
+// console.log(Math.floor(4.9))
+// console.log(Math.min(4,5,8,9,2))
+// console.log(Math.max(4,5,8,9,2))
+// console.log(Math.random())
+// console.log((Math.random()*10) + 1)
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random()*(max - min+1 ))+ min)
