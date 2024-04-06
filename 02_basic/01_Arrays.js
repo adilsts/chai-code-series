56d41b293585f00bcba6f42cb5fff0887f8f8f10
adilsts <149329789+adilsts@users.noreply.github.com>
2024-04-06 00:22:01 +0000

    01 arrays part 1

---
+++ 02_basic/01_Arrays.js
@@ -0,0 +1,30 @@
+//array
+
+const newArr = [1,2,3,4,5]
+//  const myHeros = ["Abu Bakar", "Umer" , "Saddique"]
+//  const newArr2 = new Array (1,2,3,4,5)
+//  console.log(newArr2[3])
+// newArr.push(9)
+// newArr.pop()
+// newArr.unshift(8)
+// newArr.shift()
+// console.log(newArr.includes(9))
+// console.log(newArr.indexOf(3))
+
+// const myArr = newArr.join()
+// console.log(typeof myArr)
+// console.log(typeof newArr)
+
+// slice , splice
+
+console.log( "A" , newArr)
+const myn1 = newArr.slice(1,3)
+console.log(myn1)
+
+console.log( "B" , newArr)
+const myn2 = newArr.splice(1,3)
+console.log( "C" , newArr)
+console.log(myn2)
+  
+
+
