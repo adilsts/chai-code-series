9e385601ed8252a243f87443fd28e25969d6ee13
adilsts <149329789+adilsts@users.noreply.github.com>
2024-04-08 07:54:18 +0000

    scope 02 done

--- 03_basics/02_scops.js
+++ 03_basics/02_scops.js
@@ -5,6 +5,44 @@ if(true){
 
 }
 
-console.log(a);
-console.log(b);
-console.log(c)
+// console.log(a);
+// console.log(b);
+// console.log(c)
+
+function one(){
+    const username = "Adil"
+
+    function two(){
+        const website = "youtube"
+        console.log(username)
+    }
+// console.log(website)
+
+    two()
+}
+
+// one()
+
+if(true){
+    const username = "Adil khan"
+    if(username === "Adil khan"){
+        const website = "facebook"
+        // console.log(username + website)
+    }
+    // console.log(website);
+}
+
+// console.log(username)
+
+//+=+=+++++++++++++++interesting+++++++++++++//
+console.log(addone(5));
+function addone(num){
+    return num + 1
+}
+
+
+
+addTwo(5)
+const addTwo = function (num){
+    return num+2
+}
