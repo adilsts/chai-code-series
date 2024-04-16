efeed9ed27677f2914f9de4ead1886dfe7bf027d
adilsts <149329789+adilsts@users.noreply.github.com>
2024-04-16 05:07:20 +0000

    control flow done

---
+++ 04_controlflow/one.js
@@ -0,0 +1,25 @@
+// if
+// const isuserLoggedin = true
+// const tempreature = 41
+// if (tempreature === 41) {
+//     console.log("less than 50")
+// } else {
+//     console.log("tempreature greature than 50")
+// }
+// console.log("executed");
+
+// const score = 200;
+
+// if(score > 100 ) {
+//     const power = "fly";
+//     console.log(`User power: ${power}`)
+// }
+
+const balace = 1000;
+
+// if (balace > 500) console.log( `balancee is greater than  `),console.log( `balancee is smaller than  `)
+
+
+
+
+
